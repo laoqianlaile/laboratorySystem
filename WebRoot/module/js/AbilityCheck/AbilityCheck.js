850f14b1dbb898a14a921d11f997f03e1def3a48
dr520 <976104701@qq.com>
2016-12-20 22:43:09 +0800

    能力验证

--- WebRoot/module/js/AbilityCheck/AbilityCheck.js
+++ WebRoot/module/js/AbilityCheck/AbilityCheck.js
@@ -1,4 +1,5 @@
 
+
 var object= {};
 	pubtype = "";
 	datacount = -1;
@@ -13,7 +14,7 @@ function queryParams(pageReqeust) {
 	pageReqeust.pageNumber = 1;
 	pageReqeust.pageSize = 9;
 	pageReqeust.searchText = "";
-	pageReqeust.sortName = 'roleID,equipmentName';
+	pageReqeust.sortName = 'employeeName,equipmentName';
 	pageReqeust.sortOrder = 'asc';
 	pageReqeust.condition = condition;
 
@@ -32,7 +33,7 @@ function initialize(){
 		pageNumber:1,//首页页码
 		pageList: [3, 5, 9, 10, 200, 500],//设置可供选择的页面数据条数
 		cache: false,//禁用 AJAX 数据缓存
-		sortName:'roleID,equipmentName',//定义排序列
+		sortName:'employeeName,equipmentName,startTime',//定义排序列
 		sortOrder:'asc',//定义排序方式
 		url:'abilityCheckController/getAbilityCheckWithPaging.do',//服务器数据的加载地址
 		sidePagination:'server',//设置在哪里进行分页
@@ -46,6 +47,7 @@ function initialize(){
 			width : '5%'// 宽度
 		}, {
 			field : 'ID',// 返回值名称
+			width:"17%",
 			visible:false
 		}, {
 			field : 'parameter',// 返回值名称
@@ -54,7 +56,7 @@ function initialize(){
 			valign : 'middle',// 垂直居中显示
 			width : '10%'// 宽度
 		}, {
-			field : 'roleID',// 返回值名称
+			field : 'employeeName',// 返回值名称
 			title : '组织者',// 列名
 			align : 'center',// 水平居中显示
 			valign : 'middle',// 垂直居中显示
@@ -197,12 +199,7 @@ function onDblClickCell(field,value,row,$element){
 		;break;
 		
 		case "startTime":getvalue = row.startTime;
-		if(row.startTime == "请初始化日期")
-		$element[0].innerHTML = "<input id='in123' class='form-control' value='' type='text' />";   
-		else
 		$element[0].innerHTML = "<input id='in123' class='form-control' value='"+getvalue+"' type='text' />";   
-		timeDom = $('#in123');
-
 		$("#in123").datetimepicker({
 	        minView: 'month',            //设置时间选择为年月日 去掉时分秒选择
 	        format:'yyyy-mm',
@@ -214,7 +211,7 @@ function onDblClickCell(field,value,row,$element){
 	        language: 'zh-CN'              //设置时间控件为中文
 	    }).on('changeDate', function(ev){
 	    	var val = $('#in123').val();
-	    	document.getElementById("in123").parentNode.innerHTML = $('#in123').val();
+			document.getElementById("in123").parentNode.innerHTML = $('#in123').val();
 	    	row.startTime = val;
 		}); 
 		/**/
@@ -239,30 +236,29 @@ function add(){
 	document.getElementById('uploading').style.display ="block";
 	$('#table').bootstrapTable('insertRow',{index:0,row:{
 		parameter:"",
-		roleID :"",
+		employeeName :"",
 		type:"",
 		equipmentName:"",
 		departmentName:"",
 		startTime:"",
 		result:"",
 		state:'未完成',
-		fileID:"",
+		fileName:"",
 	}});
 	
 }
 
 function addPlan(dom){
 	var getdata = $('#table').bootstrapTable('getSelections');
-	if(getdata[0].parameter == "" || getdata[0].equipmentName == "" || getdata[0].departmentName == "" || getdata[0].startTime == ""){
+	if(getdata[0].parameter == "" || getdata[0].equipmentName == "" || getdata[0].departmentName == ""){
 		 $.globalMessenger().post({
-	  		   message: "参数，设备名称，部门名称,实施日期必填",//提示信息
+	  		   message: "参数，设备名称，部门名称必填",//提示信息
 	  		   type: 'error',//消息类型。error、info、success
 	  		   hideAfter: 2,//多长时间消失
 	  		   showCloseButton:true,//是否显示关闭按钮
 	  		   hideOnNavigate: true //是否隐藏导航
-	  		 });
+	  		 });	
 	}
-	else{
 	if(getdata.length<=0||getdata.length > 1){
 		 $.globalMessenger().post({
   		   message: "请选择一条数据",//提示信息
@@ -276,10 +272,7 @@ function addPlan(dom){
 	dataobj.parameter = getdata[0].parameter;
 	dataobj.equipmentID = reciveID.equipmentID;
 	dataobj.departmentID = reciveID.departmentID;
-	if(timeDom == "123")
-		dataobj.startTime = "1010-10-01 00:00:00";
-	else
-		dataobj.startTime = timeDom.val() + "-1 00:00:00";
+		dataobj.startTime = getdata[0].startTime + "-1 00:00:00";
 	dataobj.state = "未完成";
 	$.ajax({
 		url:'abilityCheckController/addAbilityCheck.do',
@@ -306,7 +299,6 @@ function addPlan(dom){
 	  		   hideOnNavigate: true //是否隐藏导航
 	  		 });
 	}
-	}
 }
 function deleteModal(){
 	var getdata = $('#table').bootstrapTable('getSelections');
@@ -325,7 +317,6 @@ function deleteModal(){
 
 function deletePlan(){
 		var getdata = $('#table').bootstrapTable('getSelections');
-		
 		var Ids = new Array();
 		for(var i = 0; i < getdata.length; i++){
 			Ids[i] = getdata[i].ID;
@@ -363,22 +354,13 @@ function editPlan(){
 	  		   hideOnNavigate: true //是否隐藏导航
 	  		 });
 	}
-	else if(updata[0].ID == "" || updata[0].ID == null){
-		$.globalMessenger().post({
-	  		   message: "请先上传数据，再进行修改",//提示信息
-	  		   type: 'error',//消息类型。error、info、success
-	  		   hideAfter: 2,//多长时间消失
-	  		   showCloseButton:true,//是否显示关闭按钮
-	  		   hideOnNavigate: true //是否隐藏导航
-	  		 });
-	}
+
 	else{
 		data.ID = updata[0].ID;
-		data.employeeID = updata[0].roleID;
 		data.parameter = updata[0].parameter;
 		data.equipmentID = reciveID.equipmentID;
 		data.departmentID = reciveID.departmentID;
-		data.startTime = updata[0].startTime;
+		data.startTime = updata[0].startTime+"-01 00:00:00";
 
 			data.result = updata[0].result;
 			data.state = updata[0].state;
@@ -426,12 +408,13 @@ function find(){
 	var employeeID = $('#Organizer').val();
 	var equipmentID = $('#Devicename').val();
 	var startTime = $('#Executiontime').val();
+	var endTime = $('#Executiontime2').val();
 	var type = $('#Implement').val();
 	if(employeeID != null && employeeID != ""){
 		if(condition == "")
-			condition += " roleID = '" + employeeID + "'";
+			condition += " employeeName = '" + employeeID + "'";
 		else
-		condition += " and roleID = '" + employeeID + "'";
+		condition += " and employeeName = '" + employeeID + "'";
 	}
 	if(equipmentID != null && equipmentID != ""){
 		if(condition == "")
@@ -439,13 +422,30 @@ function find(){
 		else
 		condition += " and equipmentName = '" + equipmentID + "'";
 	}
-	if(startTime != null && startTime != ""){
+	if((startTime != null && startTime != "") && (endTime == "")){
+		alert("111");
 		startTime += "-01 00:00:00";
 		if(condition == "")
 			condition += " startTime = '" + startTime + "'";
 		else
 		condition += " and startTime = '" + startTime + "'";
 	}
+	 if((startTime == "") && (endTime != null && endTime != "")){
+		endTime += "-01 00:00:00";
+		if(condition == "")
+			condition += " startTime = '" + endTime + "'";
+		else
+		condition += " and startTime = '" + endTime + "'";
+	}
+	 if((startTime != null && startTime != "") && (endTime != null && endTime != "")){
+		startTime += "-01 00:00:00";
+		endTime += "-01 00:00:00";
+		if(condition == "")
+			condition += " startTime   between '" + startTime + "' and '" + endTime + "'";
+		else
+		condition += " startTime   between '" + startTime + "' and '" + endTime + "'";
+	}
+	
 	if(type != null && type != ""){
 		if(condition == "")
 			condition += " abilitycheck.type = '" + type + "'";
@@ -472,6 +472,7 @@ function checkTable(){
 
 /*审查*/
 function check(){
+	var file = $('#file').val();
 	var data = $('#table').bootstrapTable('getSelections');
 	var checkdata = {};
 	checkdata.ID = data[0].ID;
@@ -479,30 +480,23 @@ function check(){
 	checkdata.result = $('#check_Result').val();
 	checkdata.type = $('#get_Type').val();
 	
-	if(checkdata.fileID == null || checkdata.fileID == ""){
+	if(file!=""){
 	checkdata.fileID = ""+date.getFullYear()+date.getMonth()+date.getDate()+date.getHours()+date.getMinutes()+date.getSeconds()+date.getMilliseconds();
 	$('#cID').val(checkdata.fileID);
-	}	
-	else{
-		$('#cID').val(data[0].fileID);
-		checkdata.fileID = data[0].fileID;
-		$('#cstate').val("1");	
 	}
 	checkdata.state = "已完成";
-	
-	alert(checkdata.fileID);
 	var update = {};
-	update.employeeID = data[0].roleID;
+	update.fileID = checkdata.fileID;
 	update.equipmentID = data[0].equipmentID;
 	update.startTime = data[0].startTime + "-1 00:00:00";
-	update.fileID = checkdata.fileID;
+	if(file != null && file !=""){
 	$.ajax({
 			url:'abilityCheckController/updateAbilityCheckByCondition.do',
 			data:update,
 			success:function(e){
 			}
 	});
-	
+	}
 	$.ajax({
 		  url:'abilityCheckController/updateAbilityCheck.do',
 		  data:checkdata,
@@ -517,7 +511,15 @@ function check(){
 			  		 });
 		  }
 		});
+
+	if(file != "" && file != null){
+	var form = document.forms[0];
+		form.submit();
+	}
+	$('#file').val("");
 	$('#checkModal').modal('hide');
+	
+
 }
 
 /*文件下载*/
@@ -554,5 +556,15 @@ $("#Executiontime").datetimepicker({
     startView:3,
     minView:3,
     language: 'zh-CN'
+});
+$("#Executiontime2").datetimepicker({
+	minView: 'month',            //设置时间选择为年月日 去掉时分秒选择
+	format:'yyyy-mm',
+	todayBtn:"linked",
+	autoclose:true,
+	todayHighlight:true,
+	startView:3,
+	minView:3,
+	language: 'zh-CN'
 });
 
