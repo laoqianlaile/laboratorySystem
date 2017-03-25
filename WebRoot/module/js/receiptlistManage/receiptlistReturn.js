d641c99fa7083774cbdb3c08fee073e42d78c170
hezengzhen <2551065956@qq.com>
2017-03-25 17:17:22 +0800

    BUG

--- WebRoot/module/js/receiptlistManage/receiptlistReturn.js
+++ WebRoot/module/js/receiptlistManage/receiptlistReturn.js
@@ -1,45 +1,34 @@
-var reID, conID, comID, state;
-state = getUrlParam("state");
+//var reID, conID, comID, state;
+//state = getUrlParam("state");
+
+var reID = getUrlParam("reID");
+var coID = getUrlParam("coID");
+var comID = getUrlParam("comID");
+var coCode = getUrlParam("coCode");
+var state = getUrlParam("state");
+var reCode = getUrlParam("reCode");
 
 $(function() {
 	initPageData();
 });
 
 function initPageData() {
-	if (state == "add") {
-		init();
-		initSample();
-	}
+	init();
+}
 
-	if (state == "edit") {
-		reID = getUrlParam("reID");
-
+function init() {
+	if(state=="edit"){
 		$("#receiptlistID").val(reID);
 		getReceiptlistInforInReturn(reID);
-		initSample();
+	}else if(state=="add"){
+		$("#receiptlistID").val(reID);
+		$("#show_receiptlistCode").val(reCode);
+		$("#show_contractCode").val(coCode);
+		$("#show_contractCode").attr("disabled", true);
+		getCompanyName();
 	}
+	initSample();
 }
-
-function getReceiptlistInforInReturn(reID) {
-	$.ajax({
-		url : 'receiptlistController/getReceiptlistInforInReturn.do',
-		data : {
-			"ID" : reID
-		},
-		dataType : 'json',
-		success : function(o) {
-			var data = JSON.parse(o);
-			$('#show_receiptlistCode').val(data[0].receiptlistCode);
-			$('#show_contractCode').val(data[0].contractCode);
-			$("#show_contractCode").attr("disabled", true);
-			$('#show_linkMan').val(data[0].linkMan);
-			$('#show_companyName').val(data[0].companyName);
-			$("#show_companyName").attr("disabled", true);
-			$('#show_linkPhone').val(data[0].linkPhone);
-		}
-	});
-}
-
 function initSample() {
 	$('#sampleTable')
 			.bootstrapTable(
@@ -85,7 +74,7 @@ function initSample() {
 									title : '出厂编号',// 列名
 									align : 'center',// 水平居中显示
 									valign : 'middle',// 垂直居中显示
-									width : '18%'// 宽度
+									width : '20%'// 宽度
 								},
 								{
 									field : 'ID',// 返回值名称
@@ -116,70 +105,39 @@ function initSample() {
 									title : '名称',// 列名
 									align : 'center',// 水平居中显示
 									valign : 'middle',// 垂直居中显示
-									width : '18%'// 宽度
+									width : '20%'// 宽度
 								},
 								{
 									field : 'specifications',// 返回值名称
 									title : '型号/规格/代号',// 列名
 									align : 'center',// 水平居中显示
 									valign : 'middle',// 垂直居中显示
-									width : '18%'// 宽度
-								},
-								{
-									field : 'testName',// 返回值名称
-									title : '检测/校准项目',// 列名
-									align : 'center',// 水平居中显示
-									valign : 'middle',// 垂直居中显示
-									width : '18%'// 宽度
+									width : '20%'// 宽度
 								},
 								{
 									field : 'createTime',// 返回值名称
 									title : '录入时间',// 列名
 									align : 'center',// 水平居中显示
 									valign : 'middle',// 垂直居中显示
-									width : '18%'// 宽度
+									width : '20%'// 宽度
 								},
 								{
 									field : 'operate',// 返回值名称
 									title : '操作',// 列名
 									align : 'center',// 水平居中显示
 									valign : 'middle',// 垂直居中显示
-									width : '10%',// 宽度
+									width : '20%',// 宽度
 									formatter : function(value, row, index) {
-										var btn_show = '<button type="button" onclick="showModal('
-												+ row.ID
-												+ ',\''
-												+ row.factoryCode
-												+ '\',\''
-												+ row.sampleName
-												+ '\',\''
-												+ row.specifications
-												+ '\',\''
-												+ row.testName
-												+ '\',\''
-												+ row.createTime
-												+ '\')" class="btn btn-primary glyphicon glyphicon-show">&nbsp;查看</button>&nbsp';
-										var btn_edit = '<button type="button" onclick="edit('
-												+ row.ID
-												+ ',\''
-												+ row.testID
-												+ '\',\''
-												+ row.factoryCode
-												+ '\',\''
-												+ row.sampleName
-												+ '\',\''
-												+ row.specifications
-												+ '\',\''
-												+ row.testName
-												+ '\',\''
-												+ row.createTime
-												+ '\')" class="btn btn-primary glyphicon glyphicon-edit">&nbsp;编辑</button>&nbsp';
-										var btn_del = '<button type="button" onclick="del('
-												+ row.ID
-												+ ',\''
-												+ row.taskID
-												+ '\')" class="btn btn-primary glyphicon glyphicon-remove">&nbsp;删除</button>&nbsp';
-										return btn_show + btn_edit + btn_del;
+										var btn_edit;
+										var btn_view;
+										var btn_dele;
+										btn_edit = "<input type=\"image\" src=\"module/img/receiptlistManage/edit_icon.png\" style=\"margin-left:10px;\"  onclick='openEdit("
+												+ JSON.stringify(row) + ")'>";
+										btn_view = "<input type=\"image\" src=\"module/img/receiptlistManage/view_icon.png\" style=\"margin-left:10px;\"  onclick='showModal("
+												+ JSON.stringify(row) + ")'>";
+										btn_dele = "<input type=\"image\" src=\"module/img/receiptlistManage/delete_icon.png\" style=\"margin-left:10px;\"  onclick='del("
+												+ JSON.stringify(row) + ")'>";
+										return btn_edit + btn_view + btn_dele;
 									}
 								} ]
 					// 列配置项,详情请查看 列参数 表格
@@ -187,34 +145,31 @@ function initSample() {
 					});
 }
 
-function init() {
+function getReceiptlistInforInReturn(reID) {
 	$.ajax({
-		url : 'receiptlistController/addReceiptListInReturn.do',
-		scriptCharset : "utf-8",
-		async : false,
-		contentType : "application/x-www-form-urlencoded; charset=utf-8", // 中文乱码
+		url : 'receiptlistController/getReceiptlistInforInReturn.do',
+		data : {
+			"ID" : reID
+		},
 		dataType : 'json',
 		success : function(o) {
 			var data = JSON.parse(o);
-			$("#receiptlistID").val(data.reID);
-			$("#show_receiptlistCode").val(data.reCode);
+			$('#show_receiptlistCode').val(data[0].receiptlistCode);
+			$('#show_contractCode').val(data[0].contractCode);
+			$("#show_contractCode").attr("disabled", true);
+			$('#show_linkMan').val(data[0].linkMan);
+			$('#show_companyName').val(data[0].companyName);
+			$('#show_createTime').val(data[0].createTime);
+			$("#show_companyName").attr("disabled", true);
+			$('#show_linkPhone').val(data[0].linkPhone);
 		}
 	});
 }
 
-$(document).on("blur", "#show_contractCode", function() {
-	if (state == "add") {
-		if ($("#show_contractCode").val() == "")
-			alert("样品编号不能为空");
-		if ($("#show_contractCode").val() != "")
-			isExit($("#show_contractCode").val());
-	}
-});
-
-function isExit(contractCode) {
+function getCompanyName() {
 	var parame = {};
 	var data;
-	parame.contractCode = contractCode;
+	parame.contractCode = coCode;
 	$.ajax({
 		url : 'contractController/getcompanyInforByCode.do',
 		scriptCharset : "utf-8",
@@ -226,51 +181,44 @@ function isExit(contractCode) {
 			if (typeof data[0] !== 'undefined') {
 				$("#show_companyName").val(data[0].companyName);
 				$("#show_companyName").attr("disabled", true);
-				conID = data[0].conID;
-			} else {
-				alert("非法合同编号");
 			}
 		}
 	});
 }
-
 // 查看样品信息
-function showModal(ID, factoryCode, sampleName, specifications, testName,
-		createTime) {
+function showModal(data) {
 	// console.log(factoryCode);
 	// console.log(createTime) ;
 	$('#showModal').modal('show');
-	$('#show_factoryCode').val(factoryCode);
-	$('#show_sampleName').val(sampleName);
-	$('#show_specifications').val(specifications);
-	$('#show_nameCn').val(testName);
-	$('#show_createTimes').val(createTime);
+	$('#show_factoryCode').val(data.factoryCode);
+	$('#show_sampleName').val(data.sampleName);
+	$('#show_specifications').val(data.specifications);
+	$('#show_createTimes').val(data.createTime);
 	$('#showModal input').attr("disabled", "disabled");
 }
 
 // 修改样品信息
-function edit(ID,testID, factoryCode, sampleName, specifications,
-		testName, createTime) {
+function openEdit(data) {
 	$('#editModal').modal('show');
-	$('#edit_factoryCode').val(factoryCode);
-	$('#edit_sampleName').val(sampleName);
-	$('#edit_specifications').val(specifications);
-	$('#edit_nameCn').val(testName);
-	$('#edit_createTimes').val(createTime);
+	$('#edit_factoryCode').val(data.factoryCode);
+	$('#edit_sampleName').val(data.sampleName);
+	$('#edit_specifications').val(data.specifications);
+	$('#edit_nameCn').val(data.testName);
+	$('#edit_createTimes').val(data.createTime);
+	$('#edit_ID').val(data.ID);
+}
 
-	$('#edit').click(function() {
-		var parame = {};
-		parame.ID = ID;
-		parame.testID = testID;
-		parame.factoryCode = $('#edit_factoryCode').val();
-		parame.sampleName = encodeURI($('#edit_sampleName').val());
-		parame.specifications = $('#edit_specifications').val();
-		parame.nameCn = encodeURI($('#edit_nameCn').val());
-		parame.createTime = $('#edit_createTimes').val();
-        if(checkSampledata(parame)){
+function edit() {
+	var parame = {};
+	parame.factoryCode = $('#edit_factoryCode').val();
+	parame.sampleName = encodeURI($('#edit_sampleName').val());
+	parame.specifications = $('#edit_specifications').val();
+	parame.createTime = $('#edit_createTimes').val();
+	parame.ID = $('#edit_ID').val();
+	if (checkSampledata(parame)) {
 		$.ajax({
 			url : 'taskController/updReceiptlistSampleInForInReturn.do',
-			scriptCharset: "utf-8",
+			scriptCharset : "utf-8",
 			contentType : "application/x-www-form-urlencoded; charset=utf-8", // 中文乱码
 			data : parame,
 			dataType : 'json',
@@ -282,8 +230,7 @@ function edit(ID,testID, factoryCode, sampleName, specifications,
 				refresh();
 			}
 		});
-        }
-	});
+	}
 }
 
 function checkSampledata(data) {
@@ -302,16 +249,11 @@ function checkSampledata(data) {
 		return false;
 	}
 
-	if (data.nameCn == "" || data.nameCn == "null") {
-		alert("检测/校准项目不能为空");
-		return false;
-	}
-
 	if (data.createTime == "" || data.createTime == "null") {
 		alert("录入时间不能为空");
 		return false;
 	}
-	
+
 	return true;
 
 }
@@ -322,10 +264,10 @@ function refresh() {
 }
 
 // 删除交接单中的样品
-function del(ID, taskID) {
+function del(data) {
 	var parame = {};
 	// parame.ID=ID;
-	parame.taskID = taskID;
+	parame.taskID = data.taskID;
 	var isDelete = confirm("确认删除");
 	if (isDelete == true) {
 		$.ajax({
@@ -344,7 +286,7 @@ function del(ID, taskID) {
 
 // 扫描录入样品
 function automaticadd() {
-	var qrcode = 1;
+	var qrcode;
 	var ID;
 	if (qrcode == null) {
 		alert("录入失败，可选择手动录入");
@@ -357,7 +299,7 @@ function automaticadd() {
 		dataType : "json",
 		success : function(o) {
 			var data = JSON.parse(o);
-			ID = data[0].ID;
+			ID = data.ID;
 			add(ID);
 		}
 	});
@@ -370,12 +312,36 @@ function manualadd() {
 }
 
 function addSample() {
-	ID = $('#add_ID').val();
-	add(ID);
+	var sampleCode = $('#add_sampleCode').val();
+	isExitSample(sampleCode);
 }
 
 // 录入样品
+function isExitSample(sampleCode) {
+	var data;
+	$.ajax({
+		url : '/laboratorySystem/sampleController/getSampleByCode.do',
+		dataType : "json",
+		type : "post",
+		contentType : 'application/x-www-form-urlencoded; charset=UTF-8',// 发送到服务器的数据编码类型
+		async : false,
+		data : {
+			sampleCode : sampleCode
+		},
+		success : function(o) {
+			var datas = JSON.parse(o);
+			var ID = datas.sampleID;
+			if (typeof ID !== 'undefined') {
+				add(ID);
+			} else {
+				alert("该样品不存在");
+			}
+		}
+	});
+}
+
 function add(ID) {
+
 	var parame = {};
 	parame.ID = ID;
 	parame.receiptlistID = $('#receiptlistID').val();
@@ -396,57 +362,27 @@ function add(ID) {
 
 // 修改交接单信息
 function sure() {
-
-	if (state == "edit") {
-		var parame = {};
-		parame.ID = $('#receiptlistID').val();
-		parame.linkMan = encodeURI($('#show_linkMan').val());
-		parame.linkPhone = $('#show_linkPhone').val();
-		parame.createTime = $('#show_createTime').val();
-		if (checklistData(parame)) {
-			$.ajax({
-						url : 'receiptlistController/updReceiptlistInforInReturn.do',
-						contentType : "application/x-www-form-urlencoded; charset=UTF-8", // 中文乱码
-						data : parame,
-						dataType : 'json',
-						success : function(o) {
-							if (o <= 0) {
-								alert("修改失败");
-							}
-						}
-					});
-			 window.location.href =
-			 window.location.href.split("?")[0].replace(
-			 'receiptlistReturn.jsp', 'receiptlistManage.jsp');
-		}
+	var parame = {};
+	parame.ID = $('#receiptlistID').val();
+	parame.linkMan = encodeURI($('#show_linkMan').val());
+	parame.linkPhone = $('#show_linkPhone').val();
+	parame.createTime = $('#show_createTime').val();
+	if (checklistData(parame)) {
+		$.ajax({
+			url : 'receiptlistController/updReceiptlistInforInReturn.do',
+			contentType : "application/x-www-form-urlencoded; charset=UTF-8", // 中文乱码
+			data : parame,
+			dataType : 'json',
+			success : function(o) {
+				if (o <= 0) {
+					alert("修改失败");
+				}else{
+					window.location.href = window.location.href.split("?")[0].replace(
+							'receiptlistReturn.jsp', 'receiptlistManage.jsp');
+				}
+			}
+		});
 	}
-
-	if (state == "add") {
-		var parame = {};
-		parame.reID = $('#receiptlistID').val();
-		parame.linkMan = encodeURI($('#show_linkMan').val());
-		parame.linkPhone = $('#show_linkPhone').val();
-		parame.createTime = $('#show_createTime').val();
-		parame.conID = conID;
-		if (checklistData(parame)) {
-			$
-					.ajax({
-						url : 'receiptlistController/updRelistInforInReturn.do',
-						contentType : "application/x-www-form-urlencoded; charset=UTF-8", // 中文乱码
-						data : parame,
-						dataType : 'json',
-						success : function(o) {
-							if (o <= 0) {
-								alert("操作失败");
-							}
-						}
-					});
-			 window.location.href =
-			 window.location.href.split("?")[0].replace(
-			 'receiptlistReturn.jsp', 'receiptlistManage.jsp');
-		}
-	}
-
 }
 
 function checklistData(data) {
@@ -467,11 +403,7 @@ function checklistData(data) {
 		alert("录入时间不能为空");
 		return false;
 	}
-	if (data.conID == "" || data.conID == "null") {
-		alert("合同编号不能为空");
-		return false;
-	}
-	
+
 	return true;
 }
 
@@ -495,7 +427,7 @@ function checkeditdata(data) {
 		alert("录入时间不能为空");
 		return false;
 	}
-	
+
 	return true;
 }
 
@@ -520,10 +452,10 @@ function dele() {
 
 	if (state == "edit") {
 		window.location.href = window.location.href.split("?")[0].replace(
-			'receiptlistReturn.jsp', 'receiptlistManage.jsp');
+				'receiptlistReturn.jsp', 'receiptlistManage.jsp');
 	}
 
-	if (state =="add") {
+	if (state == "add") {
 		var parame = {};
 		parame.reID = $('#receiptlistID').val();
 		$.ajax({
@@ -533,8 +465,8 @@ function dele() {
 			success : function(o) {
 				if (o == true) {
 					window.location.href = window.location.href.split("?")[0]
-						.replace('receiptlistReturn.jsp',
-								'receiptlistManage.jsp');
+							.replace('receiptlistReturn.jsp',
+									'receiptlistManage.jsp');
 				}
 			}
 		});
@@ -547,4 +479,31 @@ function getUrlParam(name) {
 	if (r != null)
 		return decodeURI(r[2]);
 	return null;
+}
+
+function checkData(data) {
+	if (!data.hasOwnProperty("receiptlistCode") || data.receiptlistCode == null
+			|| data.ID.trim() == "NULL") {
+		data.receiptlistCode = "";
+	}
+	if (!data.hasOwnProperty("contractCode") || data.contractCode == null
+			|| data.contractCode.trim() == "NULL") {
+		data.contractCode = "";
+	}
+
+	if (!data.hasOwnProperty("linkMan") || data.linkMan == null
+			|| data.linkMan.trim() == "NULL") {
+		data.linkMan = "";
+	}
+
+	if (!data.hasOwnProperty("companyName") || data.companyName == null
+			|| data.companyName.trim() == "NULL") {
+		data.companyName = "";
+	}
+
+	if (!data.hasOwnProperty("linkPhone") || data.linkPhone == null
+			|| data.linkPhone.trim() == "NULL") {
+		data.linkPhone = "";
+	}
+
 }
