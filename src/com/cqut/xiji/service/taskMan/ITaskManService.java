63a3758cedf29780aa4333c5e938006d0b617505
cccyb <chenyubo1996@gmail.com>
2017-07-01 16:29:06 +0800

    分配工时功能修改

--- src/com/cqut/xiji/service/taskMan/ITaskManService.java
+++ src/com/cqut/xiji/service/taskMan/ITaskManService.java
@@ -1,5 +1,8 @@
 package com.cqut.xiji.service.taskMan;
 
+import java.util.Map;
+
 public interface ITaskManService {
-	
+	public Map<String, Object> getTaskManWithPaging(String ID, int limit, int offset, String sort, String order);
+	public int assignLaborHour(String ID, String detector, Double laborHour);
 }
